7c223e10ec780adec8a6ea216e9808f484570a84
Michal Daniluk <mdaniluk123@gmail.com>
2013-05-14 17:45:14 +0200

    zliczanie pytan

--- ProjektZPR/createnewtest.cpp
+++ ProjektZPR/createnewtest.cpp
@@ -4,9 +4,18 @@ CreateNewTest::CreateNewTest(QWidget *parent)
 	: QWidget(parent)
 {
 	ui.setupUi(this);
+	number = 1;
+	
 }
 
 CreateNewTest::~CreateNewTest()
 {
 
+}
+void CreateNewTest::nextQuestion(){
+	number++;
+	ui.questionNumber->setText(QString::number(number));
+	QString *questions = new QString[100];
+	questions[1] = ui.questionEdit->toPlainText();
+	ui.answerCloseAEdit->setText(questions[1]);
 }
