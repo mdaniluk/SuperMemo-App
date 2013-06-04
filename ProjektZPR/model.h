5fc96de98e8581ab222752826629d9eaff2c619d
Piotr Malecki <piotrr.malecki@gmail.com>
2013-06-04 22:24:46 +0200

    bez zbednych plikw

--- ProjektZPR/model.h
+++ ProjektZPR/model.h
@@ -14,17 +14,29 @@ class Model
 public:
 	Model();
 	~Model();
+	/// used when set Next is pressed
 	void setNext(int id, std::string question, std::string answer);
+	/// to get pointer to course object
 	Course* getCurrentCourse(){ return currentCourse; }
+	/// to get pointer to start object
 	Start* getCurrentStart(){ return start; }
+	/// to get pointer to mark object
 	Mark* getCurrentMark(){ return mark; }
+	/// to save course
 	void setSaveCourse(std::string nameOfFile);
+	/// used to set list of files
 	void setListOfFiles();
+	/// used to choose course 
 	void setChooseCourse(std::string course, bool flag);
+	/// used when delete clicked
 	void deleteChooseCourse(std::string course);
+	/// when end clicked
 	void endCourseAction(std::vector<int>, std::string);
+	///used to count mark for open questions
 	void computeOpenMark(int type, std::string user, std::string correct );
+	/// used to count mark for close questions
 	void computeCloseMark(int type , vector<bool> user, vector<bool> correct );
+	/// used when continue clicked
 	void setChooseCourseContinue(std::string name);
 private:
 	Course *currentCourse;
--- ProjektZPR/start.h
+++ ProjektZPR/start.h
@@ -19,13 +19,11 @@
 #include <boost/serialization/version.hpp>
 #include <boost/serialization/split_member.hpp>
 #include <boost/serialization/utility.hpp>
-
-
 using namespace std;
 using namespace boost;
 namespace fs = ::boost::filesystem;
 typedef boost::shared_ptr<Deck> PDeck;
-// usunac niepotrzebne includy
+
 
 namespace fs = ::boost::filesystem;
 /**
@@ -39,21 +37,33 @@ class Start
 public:
 	Start();
 	~Start();
+	/// function to set lists of file in the course window
 	void setListOfFiles();
+	/// function to get lists of file
 	std::vector<std::string> getListOfFiles(){ return filesXml;}
+	/// function to get Deck object
 	Deck * getDeck() {return deck_;}
+	/// function to get PDeck object
 	PDeck getDecknew() {return decknew_;}
+	/// function to perform action after choose button clicked
 	void chooseCourse(std::string course, bool flag);
+	/// function to perform action after delete button clicked
 	void deleteCourse(std::string course);
+	/// function to count next date for each question, implementation of SM2 algorithm
 	void setNextDateForEach(vector<int> answersJudged, std::string courseName);
+	/// serialization of current object state
 	void saveToFileCurrentState(const Deck &s, const char* filename);
+	/// load state of object from file
 	void loadFromFileCurrentState(Deck &s,  const char* filename);
+	/// function to perform action after continue button clicked
 	void continueClicked(std::string name);
+	/// field to protect from unwanted behaviour of aplication
 	bool flag_;
 
 	
 
 private:
+	///vector of file which are about to be on courses list
 	std::vector<std::string> filesXml;
 	Deck *deck_;
 	PDeck decknew_;
